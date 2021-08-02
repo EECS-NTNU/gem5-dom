abdb79a6f889e10f21d0f61418e9fe8ca255d42a
Amund Bergland Kvalsvik <amund.kvalsvik@ntnu.no>
2021-08-02 11:41:34 +0200

    configs: fixed runscript
    Change-Id: I12eed69678328a485d5aa045e2eebc5b7cdd3bfc

--- dom/epic_data.py
+++ dom/epic_data.py
@@ -39,8 +39,8 @@ caches="--caches --l1d_size=64kB --l1i_size=16kB --l2_size=2MB "\
 "--l3_size=16MB --l1d_assoc=2 --l1i_assoc=2 "\
 "--l2_assoc=8 --l3_assoc=16 --cacheline_size=64"
 
-fast_forward="--fast-forward 3000000000"
-runtime="--maxinsts=1000000000"
+fast_forward="--fast-forward 1000000000"
+runtime="--maxinsts=3000000000"
 
 mkdir = f"mkdir {gem5_root}/results"
 print(os.system(mkdir))
@@ -65,7 +65,7 @@ def run_benchmark(benchmark):
 
         move_data = f"mv {stats} {results}/{b_name}_{run_num}.txt"
         os.system(move_data)
-        run_num = run_num + 1.
+        run_num = run_num + 1
 
     move_result(benchmark)
 
