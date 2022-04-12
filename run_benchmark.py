e96a18528f6915384d2d3b00789011afeb45e851
Amund Bergland Kvalsvik <amund.kvalsvik@ntnu.no>
2022-04-12 12:01:28 +0200

    configs: merge diffs
    Change-Id: I9d7da711cf0c3a03d0e3e3c0db68e0075dcdba5c

--- run_benchmark.py
+++ run_benchmark.py
@@ -29,7 +29,7 @@ stats = ["simTicks",
 
 work_root = os.getcwd()
 gem5_root=f"{work_root}"
-gem5=f"{gem5_root}/build/X86/gem5.opt"
+gem5=f"{gem5_root}/build/X86/gem5.fast"
 se=f"{gem5_root}/configs/example/se.py"
 spec_root=f"{gem5_root}/dom/x86-spec-static-ref"
 stats="m5out/stats.txt"
@@ -42,9 +42,9 @@ caches="--caches --l1d_size=32768 --l1i_size=32768 --l2_size=2097152 "\
 "--l2cache --l3_size=16MB --l1d_assoc=4 --l1i_assoc=4 "\
 "--l3cache --l2_assoc=8 --l3_assoc=16 --cacheline_size=64"
 
-mp_args="--confidence_saturation=10 "\
-"--confidence_threshold=8 --confidence_up_step=1 "\
-"--confidence_down_step=4"
+mp_args="--mp_mode --ap_mode --confidence_saturation=6 "\
+"--confidence_threshold=2 --confidence_up_step=1 "\
+"--confidence_down_step=2"
 
 fast_forward="--fast-forward 3000000000"
 runtime="--maxinsts=1000000000"
